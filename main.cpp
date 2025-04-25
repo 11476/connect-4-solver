1833e78dd1bb404184e2899d050460168c686eee
11476 <gjw3503@gmail.com>
2025-04-25 18:42:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+void display(){
 int main(){
-  
+  // connect4 is 6hx7w
+  while (true){
+    string game_state = "";
+    int cts[7];
+    fill(cts, cts+7, 6);
+    cout << "To start connect 4 game, please enter any key.\n";
+    cin.ignore();
+    
+  }
 }
